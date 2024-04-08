edaf58267deaf52f0320d46fe4ba8b39b8c4fa97
Alexandre Silva <asilva1604@outlook.com>
2024-04-08 16:19:56 +0100

    correct header

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
     function output_header() {?>
         <header>
             <a href=""><h1 id="logo-txt">SaS</h1></a>
-            <a href=""><h1>Browse Items</h1></a>
+            <a href=""><h1>Browse</h1></a>
             <input type="text" id="search-bar">
             <a href=""><h1 id="login-btn">Login</h1></a>
             <a href=""><h1 id="register-btn">Register</h1></a>
