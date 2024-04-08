27ffb092c855eb7a16b37ca3a0c095661ecc749f
Alexandre Silva <asilva1604@outlook.com>
2024-04-08 16:18:46 +0100

    add connect files

--- database/connect.php
+++ database/connect.php
@@ -1,6 +1,11 @@
 <?php
     function getDatabaseConnection(string $db) {
-        $connect_str = "sqlite:" . $db;
-        return new PDO($connect_str);
+        try {
+            $connect_str = "sqlite:" . $db;
+            return new PDO($connect_str);
+        } catch (PDOException $e) {
+            die("Error starting database with " . $e->getMessage());
+        }
+        
     }
 ?>
