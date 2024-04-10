3f9530cde67e13318a28887d5bb63772b68cdd3a
Alexandre Silva <asilva1604@outlook.com>
2024-04-10 10:44:35 +0100

    one more wip, i guess

--- login.php
+++ login.php
@@ -6,5 +6,27 @@ include_once("templates/header.php");
 output_head("Smooth As Silk");
 output_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
+<body>
+    <section>
+        <div id="login">
+            <h1>Login</h1>
+            <form action="actions/login.php" method="post">
+                <label for="email">
+                    Email
+                    <input type="email" name="email">
+                </label>
+                <label for="password">
+                    Password
+                    <input type="password" name="password">
+                </label>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+        <div id="register">
+            <form action="actions/register.php" method="post">
+
+            </form>
+        </div>
+    </section>
+</body>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <?php
     function output_header() {?>
         <header>
-            <a href=""><h1 id="logo-txt">SAS</h1></a>
+            <a href="index.php"><h1 id="logo-txt">SAS</h1></a>
             <input type="text" id="search-bar">
             <a href="login.php"><h1 id="login-btn">Login</h1></a> <!-- login page not implemented -->
             <a id="user-profile" href="profile.php"><img src="" alt="profile picture"></a> <!-- profile page not implemented -->
