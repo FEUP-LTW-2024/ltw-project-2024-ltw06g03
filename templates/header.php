743c82faf928f795fc0dc72e463830095ab3b7c2
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-04-12 19:34:53 +0100

    update search bar

--- templates/header.php
+++ templates/header.php
@@ -3,8 +3,7 @@
         <nav id="navbar">
             <ul>
                 <li><a href="index.php"><h1 id="logo-txt">SAS</h1></a></li>
-                <li><img src="./assets/search.png" alt="shopping cart"></li>
-                <li><input type="text" id="search-bar"></li>
+                <li id="search-bar"><img src="./assets/search.png" alt="search icon"><input type="text" ></li>
                 <li><a href="login.php"><h1 id="login-btn">Login</h1></a></li> <!-- login page not implemented -->
                 <li><a id="user-profile" href="profile.php"><img src="./assets/usericon.png" alt="profile picture"></a></li> <!-- profile page not implemented -->
                 <li><a href=""><img src="./assets/shopping-cart.png" alt="shopping cart"></a></li>
