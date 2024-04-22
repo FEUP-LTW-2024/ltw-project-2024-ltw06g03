70815cd12f0c3e51541ebadab27335665745f1cd
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-04-22 17:26:27 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <?php
     function output_header(string $username = "Login") {?>
-        <nav id="navbar">
+        <header id="navbar">
             <ul>
                 <li><a href="index.php"><h1 id="logo-txt">SAS</h1></a></li>
                 <li id="search-bar"><img src="./assets/search.png" alt="search icon"><input type="text" ></li>
@@ -8,6 +8,6 @@
                 <li><a id="user-profile" href="profile.php"><img src="./assets/usericon.png" alt="profile picture"></a></li> <!-- profile page not implemented -->
                 <li><a href=""><img src="./assets/shopping-cart.png" alt="shopping cart"></a></li>
             </ul>
-        </nav>
+        </header>
     <?php }
 ?>
