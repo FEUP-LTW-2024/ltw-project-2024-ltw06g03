e4839ea67e196c09370d25111148d00261562b9b
Alexandre Silva <asilva1604@outlook.com>
2024-04-22 16:40:41 +0100

    added a stupid form of login, and kind of register

---
+++ actions/login.php
@@ -0,0 +1,29 @@
+<?php
+include_once('../database/connect.php');
+if(strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
+    $email = $_POST['email'];
+    $userPassword = $_POST['password'];
+    $userPassword = sha1($userPassword);
+
+    try {
+        $db = getDatabaseConnection('../database/database.db');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $db->prepare("SELECT password FROM users WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            $password = $result['password'];
+            if ($password === $userPassword) {
+                echo 'Logged In';
+            }
+        }
+    } catch (PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+}
+?>
--- actions/register.php
+++ actions/register.php
@@ -5,7 +5,6 @@ if(strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
     $password = $_POST['password'];
     $password = sha1($password);    //encrypt password
     $name = $_POST['name'];
-    //name is not being used????
     $username = $_POST['username'];
 
     $location = '';
@@ -14,8 +13,8 @@ if(strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
         $db = getDatabaseConnection('../database/database.db');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $db->prepare("INSERT INTO users (username, email, password, location) VALUES (?, ?, ?, ?)");
-        $stmt->execute([$username, $email, $password, $location]);
+        $stmt = $db->prepare("INSERT INTO users (username, email, password, location, name) VALUES (?, ?, ?, ?, ?)");
+        $stmt->execute([$username, $email, $password, $location, $name]);
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
