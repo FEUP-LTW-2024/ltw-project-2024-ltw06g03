5d5a43923aa05d5df889444631f0441ab4b6e5e4
educunhA04 <118934693+educunhA04@users.noreply.github.com>
2024-05-04 12:18:14 +0100

    edit profile forms

--- edit-profile.php
+++ edit-profile.php
@@ -9,29 +9,26 @@ output_head("Smooth As Silk");
 <body>
     <?php output_header(); ?>
     <div class="container rounded bg-white mt-5 mb-5">
-    <div class="row">
-        <div class="col-1">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img src="./assets/user_profile.png" alt="profile picture">
-
-        <div class="col-2">
-            <div class="card">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                    <h4 class="text-right">Profile Settings</h4>
+        <div class="row">
+            <div class="card-1">
+                <img src="./assets/user_profile.png" alt="profile picture">
+                <div class="profile-basic-info">
+                    <div class="username-edit"><label class="labels">Username</label><input type="text" class="form-control" placeholder="username" value=""></div>
+                    <div class="name-edit"><label class="labels">Name</label><input type="text" class="form-control" value="" placeholder="name"></div>
                 </div>
-                <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Username</label><input type="text" class="form-control" placeholder="username" value=""></div>
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" value="" placeholder="name"></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="example@example.com" value=""></div>
-                    <div class="col-md-12"><label class="labels">Password</label><input type="password" class="form-control" placeholder="password" value=""></div>
-                    <div class="col-md-12"><label class="labels">Confirm Password</label><input type="password" class="form-control" placeholder="confirm password" value=""></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
             </div>
+            <div class="card-2">                    
+                <div class="profile-settings">
+                    <div class="email-edit"><label class="label">Email</label><input type="text" class="form-control" placeholder="example@example.com" value=""></div>
+                    <div class="password-edit"><label class="label">Password</label><input type="password" class="form-control" placeholder="password" value=""></div>
+                    <div class="confirm-password-edit"><label class="label">Confirm Password</label><input type="password" class="form-control" placeholder="confirm password" value=""></div>
+                </div>
+                <div class="save-changes"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                
+            </div>
+        
         </div>
     </div>
-</div>
     <?php output_footer(); ?>
     
 </body>
