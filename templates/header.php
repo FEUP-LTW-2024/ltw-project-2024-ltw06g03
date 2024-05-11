835f98f16385b23c4e1c3d8a29a38dd17c6a7f38
FrozenHart <filipe.neves.esteves.4@gmail.com>
2024-05-11 16:44:37 +0100

    fix header

--- templates/header.php
+++ templates/header.php
@@ -6,11 +6,11 @@
                 <li id="search-bar"><img src="./assets/search.png" alt="search icon"><input type="text" ></li>
                 <li><a href="login.php"><h1 id="login-btn">Login</h1></a></li> <!-- login page not implemented -->
             </ul>
-        </nav>
+        </header>
     <?php }
 
     function output_logged_in_header() {?>
-        <nav id="navbar">
+        <header id="navbar">
             <ul>
                 <li id="logo-li"><a href="index.php"><h1 id="logo-txt" class="barcode">SAS</h1></a></li>
                 <li id="search-bar"><img src="./assets/search.png" alt="search icon"><input type="text" ></li>
