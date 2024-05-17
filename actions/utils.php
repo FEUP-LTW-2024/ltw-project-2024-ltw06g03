2eda6a680335487ff2a6739ff104e66573b38035
Alexandre Silva <asilva1604@outlook.com>
2024-05-17 16:19:04 +0100

    add is buyer function

--- actions/utils.php
+++ actions/utils.php
@@ -23,6 +23,28 @@ function is_seller(string $email) : bool {
     return true;
 }
 
+function is_buyer(string $email) : bool {
+    $db = getDatabaseConnection('database/database.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare('SELECT id FROM users WHERE email = :email');
+    $stmt->bindParam(':email', $email);
+
+    $stmt->execute();
+    $id = $stmt->fetch(PDO::FETCH_ASSOC)['id'];
+
+    $stmt = $db->prepare('SELECT COUNT(*) AS cnt FROM buyer WHERE user_id = :id');
+    $stmt->bindParam(':id', $id);
+
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC)['cnt'];
+
+    if ($result === 0) {
+        return false;
+    }
+    return true;
+}
+
 function get_seller_name(int $seller_id) {
     $db = getDatabaseConnection('database/database.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
