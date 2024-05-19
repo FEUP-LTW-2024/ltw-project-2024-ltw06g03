f3524ab6f5b45fc504c6a3939968f27f438099c6
Alexandre Silva <asilva1604@outlook.com>
2024-05-19 16:50:29 +0100

    showing all selling items

--- seller-items.php
+++ seller-items.php
@@ -4,6 +4,8 @@ include_once("templates/header.php");
 include_once("templates/footer.php");
 include_once("database/connect.php");
 include_once("templates/footer.php");
+include_once("actions/utils.php");
+include_once("templates/product_info.php");
 
 
 output_head("Smooth As Silk");
@@ -16,5 +18,20 @@ session_start();
     } else {
         output_header(); 
     } ?>
+
+    <section id="seller-items-page" class="page">
+        <div class = "admin-box scroll">
+            <h2>My items</h2>
+            <?php
+            $seller_id = get_seller_id(get_id_from_email($_SESSION['user_email']));
+            $items = get_selling_items($seller_id);
+            foreach ($items as $item) {
+                output_product_info($item);
+            }
+            ?>
+        </div>
+    </section>
+
     <?php output_footer('seller-items'); ?>
+
 </body>
